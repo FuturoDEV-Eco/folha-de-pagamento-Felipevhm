d71d123d42a241b5f8c2872381704862d34baea7
Felipe <fvhmanhaes@gmail.com>
2024-05-19 23:21:46 -0300

    Update solution for ex06.

--- folha_pagamento/src/index.js
+++ folha_pagamento/src/index.js
@@ -1,49 +1,78 @@
 // repository:
 // https://github.com/FuturoDEV-Eco/folha-de-pagamento-Felipevhm
-const fs = require('fs');
-const PDFDocument = require('pdfkit');
-const readline = require('readline');
-const calcularINSS = require('./calculo_inss.js');
-const calcularIR = require('./calculo_imposto_renda.js');
-const calculaSalarioLiquido = require('./calculo_salario_liquido.js');
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
+const readline = require("readline");
+const calcularINSS = require("./calculo_inss.js");
+const calcularIR = require("./calculo_imposto_renda.js");
+const calculaSalarioLiquido = require("./calculo_salario_liquido.js");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question('Qual é o nome do funcionário? ', (nome) => {
-  rl.question('Qual é o CPF do funcionário? ', (cpf) => {
-    rl.question('Qual é o mês do pagamento? ', (mes) => {
-      rl.question('Qual é o salário bruto? ', (salarioBase) => {
-        rl.question('O funcionário tem descontos? (true/false) ', (respostaDescontos) => {
-          const temDescontos = respostaDescontos.toLowerCase() === 'true';
-          const outINSS = calcularINSS(salarioBase);
-          const outIR = calcularIR(salarioBase);
+rl.question("Qual é o nome do funcionário? ", (nome) => {
+  rl.question("Qual é o CPF do funcionário? ", (cpf) => {
+      rl.question("Qual é o salário bruto? ", (salarioBase) => {
+        rl.question(
+          "O funcionário tem descontos? (true/false) ",
+          (respostaDescontos) => {
+            const temDescontos = respostaDescontos.toLowerCase() === "true";
+            const outINSS = calcularINSS(salarioBase);
+            const outIR = calcularIR(salarioBase);
 
-          const dataAtual = new Date();
-          const dataFormatada = `${dataAtual.getDate()}/${dataAtual.getMonth()+1}/${dataAtual.getFullYear()}`;
+            const dataAtual = new Date();
+            const dataFormatada = `${dataAtual.getDate()}/${
+              dataAtual.getMonth() + 1
+            }/${dataAtual.getFullYear()}`;
 
-          fs.mkdirSync('./folhas_pagamento', { recursive: true });
-          const doc = new PDFDocument;
-          doc.pipe(fs.createWriteStream(`./folhas_pagamento/${nome}.pdf`));
+            console.log(`
+          Nome: ${nome}\n
+          CPF: ${cpf}\n
+          Mês do Pagamento: ${dataAtual.getMonth() + 1}\n
+          Salario Bruto: ${salarioBase}\n
+          INSS: ${outINSS}\n 
+          IR: ${outIR}\n 
+          Tem Descontos? ${temDescontos ? "Sim." : "Não."}\n
+          -------\n
+          Salario Líquido : ${calculaSalarioLiquido(
+            salarioBase,
+            outINSS,
+            outIR,
+            temDescontos
+          )}
+        `);
+            fs.mkdirSync("./folhas_pagamento", { recursive: true });
 
-          doc.text(`Nome: ${nome}`);
-          doc.text(`CPF: ${cpf}`);
-          doc.text(`Mês do Pagamento: ${mes}`);
-          doc.text(`Salario Bruto: ${salarioBase}`);
-          doc.text(`INSS: ${outINSS}`);
-          doc.text(`IR: ${outIR}`);
-          doc.text(`Tem Descontos? ${temDescontos? "Sim.":"Não."}`);
-          doc.text(`-------`);
-          doc.text(`Salario Líquido : ${calculaSalarioLiquido(salarioBase,outINSS,outIR,temDescontos)}`);
-          doc.text(`Data: ${dataFormatada}`);
+            const doc = new PDFDocument();
+            doc.pipe(fs.createWriteStream(`./folhas_pagamento/${nome}.pdf`));
 
-          doc.end();
+            doc.text(`Nome: ${nome}`);
+            doc.text(`Data: ${dataFormatada}`);
+            doc.text(`CPF: ${cpf}`);
+            doc.text(`Mês do Pagamento: ${dataAtual.getMonth() + 1}`);
+            doc.text(`Salario Bruto: ${salarioBase}`);
+            doc.text(`INSS: ${outINSS}`);
+            doc.text(`IR: ${outIR}`);
+            doc.text(`Tem Descontos? ${temDescontos ? "Sim." : "Não."}`);
+            doc.text(`-------`);
+            doc.text(
+              `Salario Líquido : ${calculaSalarioLiquido(
+                salarioBase,
+                outINSS,
+                outIR,
+                temDescontos
+              )}`
+            );
 
-          rl.close();
-        });
+            doc.end();
+
+            rl.close();
+          }
+        );
       });
-    });
   });
 });
+
+//
